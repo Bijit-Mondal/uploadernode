3eec68ce13469e5aaf808051d8a8e8e678d88da9
BMondal-dev <71638574+BMondal-dev@users.noreply.github.com>
2020-09-23 17:27:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server = http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
