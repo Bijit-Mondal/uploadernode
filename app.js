8b61b88394938f4f017e399440335c6038479669
BMondal-dev <71638574+BMondal-dev@users.noreply.github.com>
2020-09-23 17:44:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const http = require('http');
+var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
-const port = process.env.PORT || 3000;
 
-server = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
@@ -11,20 +10,16 @@ server = http.createServer(function (req, res) {
       var newpath = 'Uploads/' + files.filetoupload.name;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
-        res.write('File uploaded and moved!. Find Your File At ');
-        console.log(newpath);
-        res.write(newpath);
+        res.write('File uploaded and moved!');
         res.end();
       });
  });
   } else {
-    fs.readFile('index.html',function(err,data){
-      res.writeHead(200,{'Content-Type':'text/html'});
-      res.write(data);
-      return res.end();
-    });
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
   }
-});
-server.listen(port,() =>{
-  console.log(`Server running at port `+port);
-})
+}).listen(8080);
