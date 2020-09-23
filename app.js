6ee4485bc4aa545faa8fac1e3b72b9b1fc74f01f
BMondal-dev <71638574+BMondal-dev@users.noreply.github.com>
2020-09-23 16:49:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+const port = process.env.PORT || 3000
+
+server = http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'Uploads/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!. Find Your File At ');
+        console.log(newpath);
+        res.write(newpath);
+        res.end();
+      });
+ });
+  } else {
+    fs.readFile('index.html',function(err,data){
+      res.writeHead(200,{'Content-Type':'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }
+});
+server.listen(port,() =>{
+  console.log(`Server running at port `+port);
+})
